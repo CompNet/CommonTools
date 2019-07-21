f154e3b9ff5e2b67236f4727f675413faae8a7cc
Vincent Labatut <vince.labatut@gmail.com>
2019-07-21 09:37:32 +0200

    fixed keys

--- src/fr/univavignon/tools/file/FileNames.java
+++ src/fr/univavignon/tools/file/FileNames.java
@@ -37,6 +37,8 @@ public class FileNames
 	public static String FO_OUTPUT = "out";
 	/** Resources folder */
 	public final static String FO_RESOURCES = "res";
+		/** Folder used to store various data */
+		public final static String FO_MISC = FO_RESOURCES + File.separator + "misc";
 		/** Folder containing the XML schemas */
 		public final static String FO_SCHEMA = FO_RESOURCES + File.separator + "schemas";
 	
@@ -67,4 +69,14 @@ public class FileNames
 	public final static String EX_HTML = ".html";
 	/** Graphml file extension */
 	public final static String EX_GRAPHML = ".graphml";
+
+	/////////////////////////////////////////////////////////////////
+	// FILES		/////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////
+	/** XML schema file used for graphml  */
+	public final static String FI_GRAPHML_SCHEMA = "graphml" + FileNames.EX_SCHEMA;
+	/** XML file used to store keys */
+	public final static String FI_KEY_LIST = "keys" + FileNames.EX_XML;
+	/** XML schema file used to store keys */
+	public final static String FI_KEY_SCHEMA = "keys" + FileNames.EX_SCHEMA;
 }
