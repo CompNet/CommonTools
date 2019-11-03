e6cd76415a5a8f4147422841b68be32d50f833f0
Vincent Labatut <vince.labatut@gmail.com>
2019-11-03 17:10:54 +0100

    updated R colors

--- res/scripts/colors.R
+++ res/scripts/colors.R
@@ -11,16 +11,29 @@
 # Colors used in the plots.
 # Taken from http://colorbrewer2.org/#type=qualitative&scheme=Set1&n=9
 ###############################################################################
-COLORS <- c(
-		rgb(228,26,28,maxColorValue=255),		# red
-		rgb(55,126,184,maxColorValue=255),		# light blue
-		rgb(77,175,74,maxColorValue=255),		# green
-		rgb(152,78,163,maxColorValue=255),		# purple
-		rgb(255,127,0,maxColorValue=255),		# orange
-		rgb(166,86,40,maxColorValue=255),		# brown
-		rgb(247,129,191,maxColorValue=255),		# pink
-		rgb(153,153,153,maxColorValue=255),		# light grey
-		rgb(23,89,143,maxColorValue=255),		# dark blue
-		rgb(30,30,30,maxColorValue=255),		# dark grey
-		rgb(255,255,51,maxColorValue=255)		# yellow
+COLORS_8 <- c(
+	rgb(228,26,28,maxColorValue=255),		# red
+	rgb(55,126,184,maxColorValue=255),		# blue
+	rgb(77,175,74,maxColorValue=255),		# green
+	rgb(152,78,163,maxColorValue=255),		# purple
+	rgb(255,127,0,maxColorValue=255),		# orange
+	rgb(166,86,40,maxColorValue=255),		# brown
+	rgb(247,129,191,maxColorValue=255),		# pink
+	rgb(255,255,51,maxColorValue=255)		# yellow
+)
+
+# variant with more colors
+COLORS_12 <- c(
+	rgb(228,26,28,maxColorValue=255),		# red
+	rgb(55,126,184,maxColorValue=255),		# light blue
+	rgb(113,219,110,maxColorValue=255),		# light green
+	rgb(152,78,163,maxColorValue=255),		# purple
+	rgb(255,127,0,maxColorValue=255),		# orange
+	rgb(166,86,40,maxColorValue=255),		# brown
+	rgb(247,129,191,maxColorValue=255),		# pink
+	rgb(153,153,153,maxColorValue=255),		# light grey
+	rgb(23,89,143,maxColorValue=255),		# dark blue
+	rgb(16,125,12,maxColorValue=255),		# dark green
+	rgb(30,30,30,maxColorValue=255),		# dark grey
+	rgb(255,255,51,maxColorValue=255)		# yellow
 )
