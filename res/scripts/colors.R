60c32841c03e6593682220833ebd7d478535c8ce
Vincent Labatut <vince.labatut@gmail.com>
2019-11-03 16:51:58 +0100

    added color constants for R: added a color

--- res/scripts/colors.R
+++ res/scripts/colors.R
@@ -12,14 +12,15 @@
 # Taken from http://colorbrewer2.org/#type=qualitative&scheme=Set1&n=9
 ###############################################################################
 COLORS <- c(
-		rgb(228,26,28,maxColorValue=255),
-		rgb(55,126,184,maxColorValue=255),
-		rgb(77,175,74,maxColorValue=255),
-		rgb(152,78,163,maxColorValue=255),
-		rgb(255,127,0,maxColorValue=255),
-		rgb(166,86,40,maxColorValue=255),
-		rgb(247,129,191,maxColorValue=255),
-		rgb(153,153,153,maxColorValue=255),
-		rgb(30,30,30,maxColorValue=255),
+		rgb(228,26,28,maxColorValue=255),		# red
+		rgb(55,126,184,maxColorValue=255),		# light blue
+		rgb(77,175,74,maxColorValue=255),		# green
+		rgb(152,78,163,maxColorValue=255),		# purple
+		rgb(255,127,0,maxColorValue=255),		# orange
+		rgb(166,86,40,maxColorValue=255),		# brown
+		rgb(247,129,191,maxColorValue=255),		# pink
+		rgb(153,153,153,maxColorValue=255),		# light grey
+		rgb(23,89,143,maxColorValue=255),		# dark blue
+		rgb(30,30,30,maxColorValue=255),		# dark grey
 		rgb(255,255,51,maxColorValue=255)		# yellow
 )
