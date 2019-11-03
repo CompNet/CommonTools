d883b61e080d3fbecd59305d5a6a156567044cd3
Vincent Labatut <vince.labatut@gmail.com>
2019-11-03 16:42:48 +0100

    added color constants for R

---
+++ res/scripts/colors.R
@@ -0,0 +1,25 @@
+#############################################################################################
+# Defines functions and constants related to colors, for plots.
+# 
+# 11/2019 Vincent Labatut
+#############################################################################################
+
+
+
+
+###############################################################################
+# Colors used in the plots.
+# Taken from http://colorbrewer2.org/#type=qualitative&scheme=Set1&n=9
+###############################################################################
+COLORS <- c(
+		rgb(228,26,28,maxColorValue=255),
+		rgb(55,126,184,maxColorValue=255),
+		rgb(77,175,74,maxColorValue=255),
+		rgb(152,78,163,maxColorValue=255),
+		rgb(255,127,0,maxColorValue=255),
+		rgb(166,86,40,maxColorValue=255),
+		rgb(247,129,191,maxColorValue=255),
+		rgb(153,153,153,maxColorValue=255),
+		rgb(30,30,30,maxColorValue=255),
+		rgb(255,255,51,maxColorValue=255)		# yellow
+)
