a575cddc798e8b8e6f93c7b7c2d23bb45bfdab7b
Vincent Labatut <vince.labatut@gmail.com>
2020-06-24 11:24:13 +0200

    minor fix in R color script

--- res/scripts/colors.R
+++ res/scripts/colors.R
@@ -10,7 +10,7 @@
 ###############################################################################
 # Colors used in the plots.
 ###############################################################################
-COLORS_8 <- c(								# basic color brewer palette, see http://colorbrewer2.org/#type=qualitative&scheme=Set1&n=9
+CAT_COLORS_8 <- c(								# basic color brewer palette, see http://colorbrewer2.org/#type=qualitative&scheme=Set1&n=9
 	rgb(228,26,28,maxColorValue=255),		# red
 	rgb(55,126,184,maxColorValue=255),		# blue
 	rgb(77,175,74,maxColorValue=255),		# green
@@ -22,7 +22,7 @@ COLORS_8 <- c(								# basic color brewer palette, see http://colorbrewer2.org/
 )
 
 # variant with more colors
-COLORS_12 <- c(								# manually extended color brewer palette
+CAT_COLORS_12 <- c(								# manually extended color brewer palette
 	rgb(228,26,28,maxColorValue=255),		# red
 	rgb(55,126,184,maxColorValue=255),		# light blue
 	rgb(113,219,110,maxColorValue=255),		# light green
